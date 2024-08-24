8ad9ca08d793d0bbe94506fea2e07a7add079e0c
Anderson <anderson19lim@gmail.com>
2024-08-24 02:31:35 -0300

    :rocket: ajuste das palavras

--- front/src/components/RegisterUser.js
+++ front/src/components/RegisterUser.js
@@ -65,7 +65,7 @@ const RegisterUser = () => {
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                Create an account
+                Criar conta
               </h1>
               <form class="space-y-4 md:space-y-6" onSubmit={addUser}>
                 <div>
@@ -73,7 +73,7 @@ const RegisterUser = () => {
                     for="username"
                     class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                   >
-                    Your username
+                    Usuário
                   </label>
                   <input
                     type="text"
@@ -91,7 +91,7 @@ const RegisterUser = () => {
                     for="password"
                     class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                   >
-                    Password
+                    Senha
                   </label>
                   <input
                     type="password"
@@ -108,15 +108,15 @@ const RegisterUser = () => {
                   type="submit"
                   class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                 >
-                  Create an account
+                  Criar conta
                 </button>
                 <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                  Already have an account?{" "}
+                 Já tem uma conta?{" "}
                   <a
                     href="/login"
                     class="font-medium text-primary-600 hover:underline dark:text-primary-500"
                   >
-                    Login here
+                    Login
                   </a>
                 </p>
               </form>
